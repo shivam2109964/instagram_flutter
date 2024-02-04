cf4c65e2de8ecdcef163c8ad9fcd95274f8a7ee3
shivam_since2002 <sg2109964@gmail.com>
2024-02-04 10:21:00 +0530

    4Feb first commit

--- lib/main.dart
+++ lib/main.dart
@@ -5,25 +5,21 @@ import 'package:instagram_flutter/utils/colors.dart';
 import 'package:instagram_flutter/responsive/mobile_screen_layout.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
-  // This widget is the root of your application.
+  const MyApp({Key? key});
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      title: 'Instagram Clone ',
-      theme: ThemeData.dark().copyWith(
-        scaffoldBackgroundColor: mobileBackgroundColor,
-      ),
+      title: "Instagram Clone",
+      theme: ThemeData.dark()
+          .copyWith(scaffoldBackgroundColor: mobileBackgroundColor),
       home: const ResponsiveLayout(
-        mobileScreenLayout: MobileScreenLayout(),
-        webScreenLayout: WebScreenLayout(),
-      ),
+          webScreenLayout: WebScreenLayout(),
+          mobileScreenLayout: MobileScreenLayout()),
     );
   }
 }
--- lib/responsive/responsive_layout_screen.dart
+++ lib/responsive/responsive_layout_screen.dart
@@ -1,26 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:instagram_flutter/utils/dimensions.dart';
 
-class ResponsiveLayout extends StatelessWidget {
-  final Widget webScreenLayout;
+class ResponsiveLayout extend StatelessWidget{
   final Widget mobileScreenLayout;
-  const ResponsiveLayout(
-      {Key? key,
-      required this.webScreenLayout,
-      required this.mobileScreenLayout})
-      : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    return LayoutBuilder(
-      builder: (context, constraints) {
-        if (constraints.maxWidth > webScreenSize) {
-          // web screen
-          return webScreenLayout;
-        }
-        //or mobile screen
-        return mobileScreenLayout;
-      },
-    );
-  }
+  final Widget webScreenLayout;
+  const ResponsiveLayout ({Key? key, required this.mobileScreenLayout, required this.webScreenLayout}) : super(key: key);
 }
